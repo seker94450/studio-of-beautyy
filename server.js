a3baef4435d7c16ee35721745125b1159214259e
seker94450 <seker94450@gmail.com>
2026-04-17 23:24:44 +0200

    Fix Gmail OAuth2 : corriger redirect URI pour correspondre au token

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ function createGmailClient() {
   const oauth2Client = new google.auth.OAuth2(
     process.env.GMAIL_CLIENT_ID,
     process.env.GMAIL_CLIENT_SECRET,
-    'urn:ietf:wg:oauth:2.0:oob'
+    'http://localhost:3030/callback'
   );
   oauth2Client.setCredentials({ refresh_token: process.env.GMAIL_REFRESH_TOKEN });
   return google.gmail({ version: 'v1', auth: oauth2Client });
